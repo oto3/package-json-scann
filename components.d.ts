8a161c58b66e387c2dc25a38e49b826e71527969
Jonathan Desjardins <jodesjard@hotmail.com>
2025-02-27 18:13:12 -0500

    feat(test-1): start testing api and create proxy to bypass cors issue

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    Home: typeof import('./src/components/home.vue')['default']
+    Home: typeof import('./src/components/Home.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,65 @@
 <template>
-  <v-container class="fill-height"/>
+  <div class="container">
+    <div class="large-12 medium-12 small-12 cell">
+      <label>File
+        <input
+          id="file"
+          ref="file"
+          type="file"
+          @:change="handleFileUpload()"
+        >
+      </label>
+      <button @:click="submitFile()">
+        Submit
+      </button>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-  //
+<script lang="ts">
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      file: "",
+      content: "",
+    };
+  },
+  methods: {
+    submitFile() {
+      console.log('File content: ', this.content)
+
+      axios
+        .post("http://localhost:3000/api/v1/query", {"version": "2.4.1", "package": {"name": "jinja2", "ecosystem": "PyPI"}}, {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+        })
+        .then(function () {
+          console.log("SUCCESS!!");
+        })
+        .catch(function () {
+          console.log("FAILURE!!");
+        });
+    },
+    handleFileUpload() {
+      this.file = this.$refs.file.files[0];
+      const reader = new FileReader();
+      if (this.file.name.includes(".json")) {
+        reader.onload = (res) => {
+          this.content = res.target.result;
+        };
+        reader.onerror = (err) => console.log(err);
+        reader.readAsText(this.file);
+      } else {
+        this.content = "check the console for file output";
+        reader.onload = (res) => {
+          console.log(res.target.result);
+        };
+        reader.onerror = (err) => console.log(err);
+        reader.readAsText(this.file);
+      }
+    },
+  },
+};
 </script>
